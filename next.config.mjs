969f64564c3524d1099971c714ed23fde0d0d80f
Grant <support@gr8result.com>
2026-02-03 07:44:45 +1000

    Update Next.js to 15.5.9 and fix config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,4 @@
-<<<<<<< HEAD
-// next.config.mjs
-const nextConfig = {
-  reactStrictMode: true,
-
-  webpack: (config) => {
-    config.watchOptions = {
-      ...(config.watchOptions || {}),
-      ignored: [
-        "**/node_modules/**",
-        "**/.git/**",
-        "C:\\hiberfil.sys",
-        "C:\\pagefile.sys",
-        "C:\\swapfile.sys",
-        "C:\\DumpStack.log.tmp",
-      ],
-    };
-    return config;
-  },
-};
-
-export default nextConfig;
-
-=======
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 export default nextConfig;
->>>>>>> origin/main
